be926c02ca028faab2b6a65a75705bee090e2c47
Mizkus <56596273+Mizkus@users.noreply.github.com>
2024-03-01 12:07:03 +0800

    init_commit

---
+++ bitArr.cpp
@@ -0,0 +1,242 @@
+#include "bitArr.h"
+#include <stdlib.h>
+
+#define BITS_IN_LONG (sizeof(long) * 8)
+
+BitArray::BitArray(int num_bits, unsigned long value) {
+    this->num_bits = num_bits;
+    max_bits = num_bits;
+
+    // Вычисляем необходимый размер массива в long-типах (8 битов в байте)
+    int num_longs = num_bits / BITS_IN_LONG + 1;
+
+    // Выделяем память для массива
+    this->data = new unsigned long[num_longs];
+
+    for (int i = 0; i < num_longs; i++) {
+        if (i == 0) data[0] = value;
+        else data[i] = 0;
+    }
+}
+
+BitArray::BitArray(const BitArray &b) {
+    *this = b;
+}
+
+BitArray::~BitArray() {
+    num_bits = 0;
+    delete[] data;
+    data = NULL;
+}
+
+void BitArray::resize(int num_bits, bool value) {
+    data = (unsigned long *) realloc(data, num_bits * sizeof(unsigned long));
+
+    if (this->num_bits <= num_bits) {
+        max_bits == num_bits ? max_bits *= 2 : max_bits = num_bits;
+        data = (unsigned long *) realloc(data, max_bits * sizeof(unsigned long));
+        for (int i = this->num_bits; i < num_bits; i++) {
+            set(i, value);
+        }
+        this->num_bits = num_bits;
+    } else {
+        max_bits = num_bits;
+        this->num_bits = num_bits;
+        data = (unsigned long *) realloc(data, num_bits * sizeof(unsigned long));
+    }
+
+}
+
+bool BitArray::operator[](int i) const {
+    int byte = data[i / BITS_IN_LONG], ind = i % BITS_IN_LONG;
+    return (byte & (1UL << ind)) != 0;
+}
+
+std::string BitArray::to_string() const {
+    std::string str = "";
+    for (int i = 0; i < num_bits; i++) str.push_back(operator[](i) + '0');
+    return str;
+}
+
+int BitArray::size() const {
+    return num_bits;
+}
+
+bool BitArray::empty() const {
+    return num_bits == 0 ? true : false;
+}
+
+void BitArray::clear() {
+    resize(0);
+}
+
+//Устанавливает бит с индексом n в значение val.
+BitArray &BitArray::set(int n, bool val) {
+    reset(n);
+    data[n / BITS_IN_LONG] |= val << n % BITS_IN_LONG;
+    return *this;
+}
+
+//Заполняет массив истиной.
+BitArray &BitArray::set() {
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) data[i] = ULONG_MAX;
+    return *this;
+}
+
+void BitArray::push_back(bool bit) {
+    if (num_bits == max_bits) resize(num_bits);
+    set(num_bits, bit);
+    num_bits++;
+}
+
+//Устанавливает бит с индексом n в значение false.
+BitArray &BitArray::reset(int n) {
+    data[n / BITS_IN_LONG] &= (ULONG_MAX ^ (1UL << n % BITS_IN_LONG));
+    return *this;
+}
+
+//Заполняет массив ложью.
+BitArray &BitArray::reset() {
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) data[i] = 0;
+    return *this;
+}
+
+//true, если массив содержит истинный бит.
+bool BitArray::any() const {
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) if (data[i] > 0) return true;
+    return false;
+}
+
+//true, если все биты массива ложны.
+bool BitArray::none() const {
+    return !any();
+}
+
+//Битовая инверсия
+BitArray BitArray::operator~() const {
+    BitArray res = *this;
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) res.data[i] = ~data[i];
+    return res;
+}
+
+//Подсчитывает количество единичных бит.
+int BitArray::count() const {
+    int c = 0;
+    for (int i = 0; i < num_bits; i++) if (operator[](i)) c++;
+    return c;
+}
+
+BitArray &BitArray::operator=(const BitArray &b) {
+    this->num_bits = b.num_bits;
+    this->max_bits = b.max_bits;
+    resize(num_bits);
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) data[i] = b.data[i];
+    return *this;
+}
+
+//Битовые операции над массивами.
+//Работают только на массивах одинакового размера.
+//Обоснование реакции на параметр неверного размера входит в задачу.
+BitArray &BitArray::operator&=(const BitArray &b) {
+    if (num_bits != b.num_bits) throw std::runtime_error("Different size of BitArrays");
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) data[i] &= b.data[i];
+    return *this;
+}
+
+BitArray &BitArray::operator|=(const BitArray &b) {
+    if (num_bits != b.num_bits) throw std::runtime_error("Different size of BitArrays");
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) data[i] |= b.data[i];
+    return *this;
+}
+
+BitArray &BitArray::operator^=(const BitArray &b) {
+    if (num_bits != b.num_bits) throw std::runtime_error("Different size of BitArrays");
+    for (int i = 0; i < num_bits / BITS_IN_LONG + 1; i++) data[i] ^= b.data[i];
+    return *this;
+}
+
+bool operator==(const BitArray &a, const BitArray &b) {
+    if (a.num_bits != b.num_bits) throw std::runtime_error("Different size of BitArrays");
+    for (int i = 0; i < a.num_bits / BITS_IN_LONG + 1; i++) {
+        if (a.data[i] != b.data[i]) return false;
+    }
+    return true;
+}
+
+bool operator!=(const BitArray &a, const BitArray &b) {
+    if (a.num_bits != b.num_bits) throw std::runtime_error("Different size of BitArrays");
+    return !operator==(a, b);
+}
+
+BitArray operator&(const BitArray &b1, const BitArray &b2) {
+    if (b1.num_bits != b2.num_bits) throw std::runtime_error("Different size of BitArrays");
+    BitArray res(b1);
+    for (int i = 0; i < b1.num_bits / BITS_IN_LONG + 1; i++) res.data[i] = b1.data[i] & b2.data[i];
+}
+
+BitArray operator|(const BitArray &b1, const BitArray &b2) {
+    if (b1.num_bits != b2.num_bits) throw std::runtime_error("Different size of BitArrays");
+    BitArray res(b1);
+    for (int i = 0; i < b1.num_bits / BITS_IN_LONG + 1; i++) res.data[i] = b1.data[i] | b2.data[i];
+}
+
+BitArray operator^(const BitArray &b1, const BitArray &b2) {
+    if (b1.num_bits != b2.num_bits) throw std::runtime_error("Different size of BitArrays");
+    BitArray res(b1);
+    for (int i = 0; i < b1.num_bits / BITS_IN_LONG + 1; i++) res.data[i] = b1.data[i] ^ b2.data[i];
+}
+
+void BitArray::swap(BitArray &b){           //works for BitArray with same sizes
+    BitArray temp = *this;
+    *this = b;
+    b = temp;
+}
+
+BitArray& BitArray::operator<<=(int n){
+    int intLen = this->num_bits / BITS_IN_LONG + 1;
+    for(int i = 0; i < intLen; i++){
+        if(i == 0) data[i] = (data[i] >> BITS_IN_LONG - n % BITS_IN_LONG);
+        else{
+            if (i - n / BITS_IN_LONG >= 0) data[i - n / BITS_IN_LONG] += (data[i] << (n % BITS_IN_LONG));
+            data[i] = (data[i] >> BITS_IN_LONG - n % BITS_IN_LONG);
+        }
+    }
+    return *this;
+}
+BitArray& BitArray::operator>>=(int n){
+    int intLen = this->num_bits / BITS_IN_LONG + 1;
+    for(int i = intLen - 1; i >= 0; i--){
+        if(i == intLen - 1) data[i] = (data[i] << BITS_IN_LONG - n % BITS_IN_LONG);
+        else{
+            if (i + n / BITS_IN_LONG < intLen) data[i + n / BITS_IN_LONG] += (data[i] >> (n % BITS_IN_LONG));
+            data[i] = (data[i] << BITS_IN_LONG - n % BITS_IN_LONG);
+        }
+    }
+    return *this;
+}
+
+BitArray BitArray::operator<<(int n) const{
+    BitArray resBitArr(*this);
+    int intLen = this->num_bits / BITS_IN_LONG + 1;
+    for(int i = 0; i < intLen; i++){
+        if(i == 0) resBitArr.data[i] = (resBitArr.data[i] >> BITS_IN_LONG - n % BITS_IN_LONG);
+        else{
+            if (i - n / BITS_IN_LONG >= 0) resBitArr.data[i - n / BITS_IN_LONG] += (resBitArr.data[i] << (n % BITS_IN_LONG));
+            resBitArr.data[i] = (resBitArr.data[i] >> BITS_IN_LONG - n % BITS_IN_LONG);
+        }
+    }
+    return resBitArr;
+}
+
+BitArray BitArray::operator>>(int n) const{
+    BitArray resBitArr(*this);
+    int intLen = this->num_bits / BITS_IN_LONG + 1;
+    for(int i = intLen - 1; i >= 0; i--){
+        if(i == intLen - 1) resBitArr.data[i] = (resBitArr.data[i] << BITS_IN_LONG - n % BITS_IN_LONG);
+        else{
+            if (i + n / BITS_IN_LONG < intLen) resBitArr.data[i + n / BITS_IN_LONG] += (resBitArr.data[i] >> (n % BITS_IN_LONG));
+            resBitArr.data[i] = (resBitArr.data[i] << BITS_IN_LONG - n % BITS_IN_LONG);
+        }
+    }
+    return resBitArr;
+}
---
+++ test.cpp
@@ -0,0 +1,214 @@
+#include "bitArr.h"
+#include "googletest/googletest/include/gtest/gtest.h"
+
+TEST(BitArrayTest, Construct) {
+BitArray bits(8, 255);
+EXPECT_EQ(bits.size(), 8);
+for (
+int i = 0;
+i < 8; i++) {
+EXPECT_TRUE(bits[i]);
+}
+
+BitArray bits1(100, ULONG_MAX);
+EXPECT_EQ(bits1.size(), 100);
+for (
+int i = 0;
+i < sizeof(long); i++) {
+if (i < sizeof(long))
+EXPECT_TRUE(bits1[i]);
+else EXPECT_FALSE(bits1[i]);
+}
+}
+
+TEST(BitArrayTest, OperationGetFromIndex){
+BitArray bits(100, ULONG_MAX);
+EXPECT_EQ(bits[0], 1);
+EXPECT_EQ(bits[31], 1);
+EXPECT_EQ(bits[32], 0);
+EXPECT_EQ(bits[99], 0);
+}
+
+TEST(BitArrayTest, Empty){
+BitArray bits1(0);
+EXPECT_TRUE(bits1.empty());
+BitArray bits2(1);
+EXPECT_FALSE(bits2.empty());
+}
+
+TEST(BitArrayTest, ToString){
+BitArray bits(10, 255);
+EXPECT_EQ(bits.to_string(), "1111111100");
+bits.resize(5);
+EXPECT_EQ(bits.to_string(), "11111");
+bits.resize(12, 0);
+EXPECT_EQ(bits.to_string(), "111110000000");
+}
+
+TEST(BitArrayTest, Resize){
+BitArray bits(10);
+bits.resize(15, 1);
+EXPECT_EQ(bits.to_string(), "000000000011111");
+}
+
+TEST(BitArrayTest, Clear){
+BitArray bits(10);
+EXPECT_FALSE(bits.empty());
+bits.
+
+clear();
+
+EXPECT_TRUE(bits.empty());
+}
+
+TEST(BitArrayTest, Set){
+BitArray bits(10);
+bits.set(8, 1);
+EXPECT_EQ(bits.to_string(), "0000000010");
+bits.
+
+set();
+
+EXPECT_EQ(bits.to_string(), "1111111111");
+}
+
+TEST(BitArrayTest, PushBack){
+BitArray bits(10);
+bits.push_back(1);
+bits.push_back(0);
+bits.push_back(1);
+EXPECT_EQ(bits.to_string(), "0000000000101");
+bits.push_back(1);
+EXPECT_EQ(bits.to_string(), "00000000001011");
+}
+
+TEST(BitArrayTest, Reset){
+BitArray bits(10);
+bits.set(8, 1);
+bits.set(9, 1);
+bits.reset(9);
+EXPECT_EQ(bits.to_string(), "0000000010");
+bits.
+
+set();
+
+bits.
+
+reset();
+
+EXPECT_EQ(bits.to_string(), "0000000000");
+}
+
+TEST(BitArrayTest, Any){
+BitArray bits(10);
+EXPECT_FALSE(bits.any());
+bits.set(1, 1);
+EXPECT_TRUE(bits.any());
+bits.
+
+reset();
+
+bits.resize(1000);
+EXPECT_FALSE(bits.any());
+bits.set(1, 999);
+EXPECT_TRUE(bits.any());
+}
+
+TEST(BitArrayTest, None){
+BitArray bits(10);
+EXPECT_TRUE(bits.none());
+bits.set(1, 1);
+EXPECT_FALSE(bits.none());
+bits.
+
+reset();
+
+bits.resize(1000);
+EXPECT_TRUE(bits.none());
+bits.set(1, 999);
+EXPECT_FALSE(bits.none());
+}
+
+TEST(BitArrayTest, Inverse){
+BitArray bits(10);
+bits = ~bits;
+EXPECT_EQ(bits.to_string(), "1111111111");
+bits.set(2, 0);
+bits = ~bits;
+EXPECT_EQ(bits.to_string(), "0010000000");
+}
+
+TEST(BitArrayTest, Count){
+BitArray bits(10);
+bits = ~bits;
+EXPECT_EQ(bits.count(), 10);
+bits.set(2, 0);
+bits = ~bits;
+EXPECT_EQ(bits.count(), 1);
+}
+
+TEST(BitArrayTest, OperationEq){
+BitArray bits(10);
+bits = ~bits;
+EXPECT_EQ(bits.to_string(), "1111111111");
+bits.set(2, 0);
+bits = ~bits;
+EXPECT_EQ(bits.to_string(), "0010000000");
+}
+
+TEST(BitArrayTest, LogicAnd){
+BitArray a(10, 1);
+BitArray b(9);
+EXPECT_THROW(a &= b, std::runtime_error);
+b.resize(10);
+a &= b;
+EXPECT_EQ(a.to_string(), "0000000000");
+}
+
+TEST(BitArrayTest, LogicOr){
+BitArray a(10, 255);
+BitArray b(9);
+EXPECT_THROW(a |= b, std::runtime_error);
+b.resize(10);
+a |= b;
+EXPECT_EQ(a.to_string(), "1111111100");
+}
+
+TEST(BitArrayTest, LogicXor){
+BitArray a(10, 255);
+BitArray b(9);
+EXPECT_THROW(a ^= b, std::runtime_error);
+b.resize(10);
+a ^= b;
+EXPECT_EQ(a.to_string(), "1111111100");
+}
+
+TEST(BitArrayTest, ShiftLeft){
+BitArray a(100, 3);
+a.set(55, 1);
+a = a << 33;
+EXPECT_EQ(a.to_string(), "0000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000");
+}
+
+TEST(BitArrayTest, EqShiftLeft){
+BitArray a(100, 3);
+a.set(55, 1);
+a <<= 33;
+EXPECT_EQ(a.to_string(), "0000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000");
+}
+
+TEST(BitArrayTest, EqShiftRight){
+BitArray a(100, 3);
+a.set(55, 1);
+a >>= 33;
+EXPECT_EQ(a.to_string(), "0000000000000000000000000000000110000000000000000000000000000000000000000000000000000010000000000000");
+}
+
+TEST(BitArrayTest, ShiftRight){
+BitArray a(100, 3);
+a.set(55, 1);
+EXPECT_EQ(a.to_string(), "0000000000000000000000000000000110000000000000000000000000000000000000000000000000000010000000000000");
+a = a >> 33;
+EXPECT_EQ(a.to_string(), "0000000000000000000000000000000110000000000000000000000000000000000000000000000000000010000000000000");
+}
+
